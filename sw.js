fd89856096a160e6dc654236af904687488ce47a
ZXCoder77 <zxcoder@hotmail.com>
2025-11-29 15:14:44 -0300

    Add initial flashcard decks and assets
    Added multiple JSON files with Japanese language flashcard decks, including numbers, days of the week, months, example phrases, and vocabulary for different modules. Also added supporting files: flashcards.html, icons, manifest.json, and sw.js to support the web application.

---
+++ sw.js
@@ -0,0 +1,48 @@
+const CACHE_NAME = 'flashcards-v1.251127';
+const ASSETS = [
+  './',
+  './flashcards.html',
+  './manifest.json',
+  './icon-192.png',
+  './icon-512.png'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => cache.addAll(ASSETS))
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request)
+      .then((response) => response || fetch(event.request))
+  );
+});
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys
+          .filter(key => key !== CACHE_NAME)
+          .map(key => caches.delete(key))
+      )
+    )
+  );
+});
+
+/*
+self.addEventListener("push", event => {
+  self.registration.showNotification("Notificação", {
+    body: event.data.text()
+  });
+});
+*/
